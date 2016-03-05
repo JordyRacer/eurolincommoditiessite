718228a3b2dbf60a5cba376c5fa5238ffa01b505
Jordan Searle <jordan@jordansearle.com>
2016-03-05 12:16:09 -0500

    all

---
+++ js/logoFade.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+$('#logoBox').hide().fadeIn(2000);
+
+$('#dateHolder').hide().fadeIn(5000);
+
+  });
---
+++ js/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+$('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+
+
+  });
+
